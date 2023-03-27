bc4d06ff1ec3634d6f997f5d9bfff76ca4caf521
Артём <av1965k@yandex.ru>
2023-03-27 23:58:28 +0400

    поработал над кастомной моделью Юзер

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
+ROLES = (
+    (USER, 'Пользователь'),
+    (MODERATOR, 'Модератор'),
+    (ADMIN, 'Администратор'),
+)
+
+
+class User(AbstractUser):
+    username = models.CharField(verbose_name='Пользователь',
+                                max_length=150,
+                                unique=True,
+                                blank=False,
+                                null=False)
+    email = models.EmailField(verbose_name='E-Mail',
+                              unique=True,
+                              max_length=254,
+                              blank=False,
+                              null=False)
+    bio = models.TextField(verbose_name='О себе',
+                           blank=True,
+                           max_length=300)
+    first_name = models.CharField(verbose_name='имя',
+                                  max_length=150,
+                                  blank=True)
+    last_name = models.CharField(verbose_name='фамилия',
+                                 max_length=150,
+                                 blank=True)
+    role = models.CharField(verbose_name='Уровень доступа',
+                            choices=ROLES,
+                            default=USER,
+                            blank=True,
+                            max_length=50)
+
+    @property
+    def is_user(self):
+        return self.role == USER
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR
+
+    def __str__(self):
+        return self.username
