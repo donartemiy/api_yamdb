6a226aff22aafc1eef019ae97ac76fa49aa5ef26
Azhur <showipbgpall2@gmail.com>
2023-03-28 22:05:17 +0500

    первая версия models без миграций, User закоментирован

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -12,43 +12,88 @@ ROLES = (
 )
 
 
-class User(AbstractUser):
-    username = models.CharField(verbose_name='Пользователь',
-                                max_length=150,
-                                unique=True,
-                                blank=False,
-                                null=False)
-    email = models.EmailField(verbose_name='E-Mail',
-                              unique=True,
-                              max_length=254,
-                              blank=False,
-                              null=False)
-    bio = models.TextField(verbose_name='О себе',
-                           blank=True,
-                           max_length=300)
-    first_name = models.CharField(verbose_name='имя',
-                                  max_length=150,
-                                  blank=True)
-    last_name = models.CharField(verbose_name='фамилия',
-                                 max_length=150,
-                                 blank=True)
-    role = models.CharField(verbose_name='Уровень доступа',
-                            choices=ROLES,
-                            default=USER,
-                            blank=True,
-                            max_length=50)
 
-    @property
-    def is_user(self):
-        return self.role == USER
+# class User(AbstractUser):
+#     username = models.CharField(verbose_name='Пользователь',
+#                                 max_length=150,
+#                                 unique=True,
+#                                 blank=False,
+#                                 null=False)
+#     email = models.EmailField(verbose_name='E-Mail',
+#                               unique=True,
+#                               max_length=254,
+#                               blank=False,
+#                               null=False)
+#     bio = models.TextField(verbose_name='О себе',
+#                            blank=True,
+#                            max_length=300)
+#     first_name = models.CharField(verbose_name='имя',
+#                                   max_length=150,
+#                                   blank=True)
+#     last_name = models.CharField(verbose_name='фамилия',
+#                                  max_length=150,
+#                                  blank=True)
+#     role = models.CharField(verbose_name='Уровень доступа',
+#                             choices=ROLES,
+#                             default=USER,
+#                             blank=True,
+#                             max_length=50)
 
-    @property
-    def is_admin(self):
-        return self.role == ADMIN
+#     @property
+#     def is_user(self):
+#         return self.role == USER
 
-    @property
-    def is_moderator(self):
-        return self.role == MODERATOR
+#     @property
+#     def is_admin(self):
+#         return self.role == ADMIN
+
+#     @property
+#     def is_moderator(self):
+#         return self.role == MODERATOR
+
+#     def __str__(self):
+#         return self.username
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Categories(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Категория')
+    slug = models.SlugField(unique=True, verbose_name='Ссылка_категории')
 
     def __str__(self):
-        return self.username
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Жанр')
+    slug = models.SlugField(unique=True, verbose_name='Ссылка_жанра')
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    # author = models.ForeignKey(
+        # User, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(
+        Categories,
+        blank=False,
+        on_delete=models.SET_NULL,
+        verbose_name='Slug категории',
+        help_text='Категория, к которой относится произведение'
+    )
+    genre = models.ForeignKey(
+        Genres,
+        blank=True,
+        # null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Slug жанра',
+        help_text='Жарн, к которому относится произведение'
+    )
+    year = models.DateTimeField(verbose_name='Год выпуска')
+
+    def __str__(self):
+        return self.name
