785b4557645cfad8307e50d26ff257bf793fbbff
Alex Efremov <alexefremov74@gmail.com>
2023-04-06 22:09:13 +0500

    Исправил модели Comment и Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 from api_yamdb.settings import (LEN_STR, LIMIT_BIO,
                                 LIMIT_EMAIL, LIMIT_NAME, LIMIT_ROLE,
-                                LIMIT_SLUG, LIMIT_USERNAME)
+                                LIMIT_SLUG, LIMIT_USERNAME,
+                                MAX_VALUE, MIN_VALUE, LIMIT_SELFTEXT)
 from .validators import validate_username, validate_year
 
 USER = 'user'
@@ -133,13 +134,7 @@ class Title(models.Model):
         return self.name
 
 
-class Review(models.Model):
-    title = models.ForeignKey(
-        Title,
-        verbose_name='Произведение',
-        on_delete=models.CASCADE,
-        related_name='reviews'
-    )
+class ReviewCommentModel(models.Model):
     text = models.TextField(
         verbose_name='Текст',
     )
@@ -149,13 +144,6 @@ class Review(models.Model):
         on_delete=models.CASCADE,
         related_name='reviews'
     )
-    score = models.PositiveSmallIntegerField(
-        verbose_name='Рейтинг',
-        validators=[
-            MinValueValidator(1, 'Допустимы значения от 1 до 10'),
-            MaxValueValidator(10, 'Допустимы значения от 1 до 10')
-        ]
-    )
     pub_date = models.DateTimeField(
         verbose_name='Дата публикации',
         auto_now_add=True,
@@ -163,9 +151,31 @@ class Review(models.Model):
     )
 
     class Meta:
+        abstract = True
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text[: LIMIT_SELFTEXT]
+
+class Review(ReviewCommentModel):
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Рейтинг',
+        validators=[
+            MinValueValidator(MIN_VALUE, 'Допустимы значения от 1 до 10'),
+            MaxValueValidator(MAX_VALUE, 'Допустимы значения от 1 до 10')
+        ]
+    )
+
+    class Meta:
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
-        ordering = ['pub_date']
+        default_related_name = 'reviews'
         constraints = [
             models.UniqueConstraint(
                 fields=['title', 'author'],
@@ -174,29 +184,15 @@ class Review(models.Model):
         ]
 
 
-class Comment(models.Model):
+class Comment(ReviewCommentModel):
     review = models.ForeignKey(
         Review,
         verbose_name='Отзыв',
         on_delete=models.CASCADE,
         related_name='comments'
     )
-    text = models.TextField(
-        verbose_name='Текст',
-    )
-    author = models.ForeignKey(
-        User,
-        verbose_name='Пользователь',
-        on_delete=models.CASCADE,
-        related_name='comments'
-    )
-    pub_date = models.DateTimeField(
-        verbose_name='Дата публикации',
-        auto_now_add=True,
-        db_index=True
-    )
 
     class Meta:
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
-        ordering = ['pub_date']
+        default_related_name = 'comments'
